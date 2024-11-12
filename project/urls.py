484a3b747e7b9f33f4faedd569df756fe387e942
pyeree <dlagusdn0218@gmail.com>
2024-11-13 00:58:42 +0900

    Feat: chat 기능 구현

--- project/urls.py
+++ project/urls.py
@@ -6,8 +6,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('community/', include('post.urls')), # 공감글
     path('pating/', include('pat.urls')),
-    #path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # 로그인 토큰 발급
-    #path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # 토큰 갱신
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # 로그인 토큰 발급
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # 토큰 갱신
     path('user/', include('user.urls')),
     path('mypage/', include('mypage.urls')),
+    path('api/', include('chat.urls')),
 ]
