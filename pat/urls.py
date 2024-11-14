f74d7ddf7b8cfb43b0ed57196a65417a7fc5775d
pyeree <dlagusdn0218@gmail.com>
2024-11-14 19:08:49 +0900

    Fix: URL api

--- pat/urls.py
+++ pat/urls.py
@@ -6,12 +6,12 @@ from .views import (
 )
 
 urlpatterns = [
-    path('posts/', PatingPostListCreateView.as_view(), name='post_list_create'),
-    path('posts/<int:pk>/', PatingPostDetailView.as_view(), name='post_detail'),
-    path('posts/<int:post_id>/join/', PatingParticipationCreateView.as_view(), name='post_join'),
-    path('posts/<int:post_id>/report/', PatingReportCreateView.as_view(), name='post_report'),
-    path('posts/<int:post_id>/block/', PatingBlockCreateView.as_view(), name='post_block'),
-    path('myposts/', MyPatingListView.as_view(), name='my_post_list'),
-    path('myposts/<int:pk>/delete/', MyPatingPostDeleteView.as_view(), name='my_post_delete'),
-    path('myposts/participated/', MyParticipatedPatingListView.as_view(), name='my_participated_post_list'),
+    path('pating/posts/', PatingPostListCreateView.as_view(), name='post_list_create'),
+    path('pating/posts/<int:pk>/', PatingPostDetailView.as_view(), name='post_detail'),
+    path('pating/posts/<int:post_id>/join/', PatingParticipationCreateView.as_view(), name='post_join'),
+    path('pating/posts/<int:post_id>/report/', PatingReportCreateView.as_view(), name='post_report'),
+    path('pating/posts/<int:post_id>/block/', PatingBlockCreateView.as_view(), name='post_block'),
+    path('pating/myposts/', MyPatingListView.as_view(), name='my_post_list'),
+    path('pating/myposts/<int:pk>/delete/', MyPatingPostDeleteView.as_view(), name='my_post_delete'),
+    path('pating/myposts/participated/', MyParticipatedPatingListView.as_view(), name='my_participated_post_list'),
 ]
--- project/urls.py
+++ project/urls.py
@@ -20,12 +20,12 @@ schema_view = get_schema_view(
 )
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('community/', include('post.urls')), # 공감글
-    path('pating/', include('pat.urls')),
+    path('api/', include('post.urls')), # 공감글
+    path('api/', include('pat.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # 로그인 토큰 발급
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # 토큰 갱신
-    path('user/', include('user.urls')),
-    path('mypage/', include('mypage.urls')),
+    path('api/', include('user.urls')),
+    path('api/', include('mypage.urls')),
     path('api/', include('chat.urls')),
     path('api/', include('noti.urls')),
 ]
