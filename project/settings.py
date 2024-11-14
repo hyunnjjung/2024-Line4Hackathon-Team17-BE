947de157b4f3e21ba2878fec5f6293a8f161f3f5
hyunnjjung <hyunnjjung@gmail.com>
2024-11-14 09:22:28 +0900

    Fix : css 적용하기 위한 정적파일 경로 수정

--- project/settings.py
+++ project/settings.py
@@ -149,7 +149,7 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT=os.path.join(BASE_DIR,'static')
+STATIC_ROOT=os.path.join(BASE_DIR,'static/')
 
 
 CORS_ALLOWED_ORIGINS = [
