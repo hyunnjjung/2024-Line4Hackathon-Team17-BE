e922c93f24850357d3b8371db5c941a20d9e9c9c
whandrea <whandrea@naver.com>
2024-11-16 00:45:01 +0900

    fix. settings urls.py

--- project/urls.py
+++ project/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # 로그인 토큰 발급
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # 토큰 갱신
     path('api/', include('user.urls')),
-    path('api/', include('mypage.urls')),
+    path('api/mypage/', include('mypage.urls')),
     path('api/', include('chat.urls')),
     path('api/', include('noti.urls')),
 ]
